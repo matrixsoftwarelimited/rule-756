6cda6f505051208ca850904caef979167e7efd3f
Selim <selim@matrixsoftware.uk>
2024-08-27 14:55:06 +0300

    Add transaction amount check and build project

--- __tests__/unit/rule.test.ts
+++ __tests__/unit/rule.test.ts
@@ -1,207 +1,127 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { handleTransaction } from '../../src';
-import { type DatabaseManagerInstance, LoggerService, CreateDatabaseManager } from '@frmscoe/frms-coe-lib';
-import { type Band, type DataCache, Rule, type RuleConfig, type RuleRequest, type RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
+import { handleTransaction } from '../../src'; // Rule-902'nin doğru yolu
+import { LoggerService } from '@frmscoe/frms-coe-lib';
+import { NetworkMap, Pacs002, type RuleConfig, type RuleRequest, type RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
 
-jest.mock('@frmscoe/frms-coe-lib', () => {
-  const original = jest.requireActual('@frmscoe/frms-coe-lib');
-  return {
-    ...original,
-    aql: jest.fn(),
-  };
-});
+const loggerService: LoggerService = new LoggerService();
 
 const getMockRequest = (amount: number): RuleRequest => {
-  const mockRule = (id: string, cfg: string, host: string): Rule => ({
-    id,
-    cfg,
-    host,
-    typologies: [],
-    getStrValue: () => `${id}:${cfg}:${host}`, // Mocked implementation
-  });
-
-  const quote: RuleRequest = {
-    transaction: {
-      TxTp: 'pacs.002.001.12',
-      FIToFIPmtSts: {
-        GrpHdr: {
-          MsgId: '6b444365119746c5be7dfb5516ba67c4',
-          CreDtTm: new Date('Mon Dec 03 2021 09:24:48 GMT+0000').toISOString(),
+  const transaction: Pacs002 = {
+    TxTp: 'pacs.002.001.12',
+    FIToFIPmtSts: {
+      GrpHdr: {
+        MsgId: 'test-message-id',
+        CreDtTm: new Date().toISOString(),
+      },
+      TxInfAndSts: {
+        OrgnlInstrId: 'test-instr-id',
+        OrgnlEndToEndId: 'test-end-to-end-id',
+        TxSts: 'ACCC',
+        ChrgsInf: [
+          {
+            Amt: {
+              Amt: amount,
+              Ccy: 'USD',
+            },
+            Agt: {
+              FinInstnId: {
+                ClrSysMmbId: {
+                  MmbId: 'test-mmb-id',
+                },
+              },
+            },
+          },
+        ],
+        AccptncDtTm: new Date(),
+        InstgAgt: {
+          FinInstnId: {
+            ClrSysMmbId: {
+              MmbId: 'instg-mmb-id',
+            },
+          },
         },
-        TxInfAndSts: {
-          OrgnlInstrId: '5ab4fc7355de4ef8a75b78b00a681ed2',
-          OrgnlEndToEndId: '2c516801007642dfb892944dde1cf845',
-          TxSts: 'ACCC',
-          ChrgsInf: [
-            {
-              Amt: { Amt: amount, Ccy: 'USD' },
-              Agt: { FinInstnId: { ClrSysMmbId: { MmbId: 'dfsp001' } } },
+        InstdAgt: {
+          FinInstnId: {
+            ClrSysMmbId: {
+              MmbId: 'instd-mmb-id',
             },
-            {
-              Amt: { Amt: 153.57, Ccy: 'USD' },
-              Agt: { FinInstnId: { ClrSysMmbId: { MmbId: 'dfsp001' } } },
-            },
-            {
-              Amt: { Amt: 30.71, Ccy: 'USD' },
-              Agt: { FinInstnId: { ClrSysMmbId: { MmbId: 'dfsp002' } } },
-            },
-          ],
-          AccptncDtTm: new Date('2021-12-03T15:36:16.000Z'),
-          InstgAgt: { FinInstnId: { ClrSysMmbId: { MmbId: 'dfsp001' } } },
-          InstdAgt: { FinInstnId: { ClrSysMmbId: { MmbId: 'dfsp002' } } },
+          },
         },
       },
     },
-    networkMap: {
-      active: true,
-      cfg: '1.0.0',
-      messages: [
-        {
-          id: '004@1.0.0',
-          host: 'test-host',
-          cfg: '1.0.0',
-          txTp: 'pacs.002.001.12',
-          typologies: [
-            {
-              id: '901@1.0.0',
-              host: 'test-host',
-              cfg: '028@1.0',
-              desc: 'Typology description',
-              rules: [mockRule('004@1.0.0', '1.0.0', 'test-host'), mockRule('028@1.0', '1.0.0', 'test-host')],
-            },
-            {
-              id: '029@1.0',
-              host: 'test-host',
-              cfg: '029@1.0',
-              desc: 'Another typology description',
-              rules: [mockRule('003@1.0', '1.0', 'test-host'), mockRule('005@1.0', '1.0', 'test-host')],
-            },
-          ],
-        },
-      ],
-    },
+    _key: 'test-key',
+  };
+
+  const networkMap: NetworkMap = {
+    active: true,
+    cfg: '1.0.0',
+    messages: [],
+  };
+
+  return {
+    transaction,
+    networkMap,
     DataCache: {
-      dbtrId: 'dbtr_516c7065d75b4fcea6fffb52a9539357',
-      cdtrId: 'cdtr_b086a1e193794192b32c8af8550d721d',
-      dbtrAcctId: 'dbtrAcct_1fd08e408c184dd28cbaeef03bff1af5',
-      cdtrAcctId: 'cdtrAcct_d531e1ba4ed84a248fe26617e79fcb64',
-      evtId: 'eventId',
-      amt: { amt: 1234.56, ccy: 'XTS' },
-      creDtTm: new Date().toISOString(),
-    },
-    metaData: {
-      prcgTmDp: Date.now(),
-      prcgTmED: Date.now(),
+      dbtrAcctId: 'test-debtor-account-id',
     },
   };
-  return quote;
 };
-
-const databaseManagerConfig = {
-  pseudonyms: {
-    certPath: '',
-    databaseName: '',
-    user: '',
-    password: '',
-    url: '',
-  },
-};
-
-let databaseManager: DatabaseManagerInstance<typeof databaseManagerConfig>;
-let ruleRes: RuleResult;
-const loggerService: LoggerService = new LoggerService();
 
 const ruleConfig: RuleConfig = {
-  id: '901@1.0.0',
+  id: '902@1.0.0',
   cfg: '1.0.0',
-  desc: 'Number of outgoing transactions - debtor',
+  desc: 'Transaction amount exceeds 100',
   config: {
     parameters: {
-      maxQueryRange: 86400000,
+      amountThreshold: 100,
     },
     exitConditions: [
       {
-        subRuleRef: '.x00',
-        reason: 'Incoming transaction is unsuccessful',
-      },
-      {
-        subRuleRef: '.x01',
-        reason: 'Transaction amount exceeds 200',
+        subRuleRef: '.ALT',
+        reason: 'Transaction amount exceeds 100',
       },
     ],
-    bands: [
-      {
-        subRuleRef: '.01',
-        upperLimit: 2,
-        reason: 'The debtor has performed one transaction to date',
-      },
-      {
-        subRuleRef: '.02',
-        lowerLimit: 2,
-        upperLimit: 4,
-        reason: 'The debtor has performed two or three transactions to date',
-      },
-      {
-        subRuleRef: '.03',
-        lowerLimit: 4,
-        reason: 'The debtor has performed 4 or more transactions to date',
-      },
-    ],
+    bands: [],
   },
 };
 
-beforeAll(async () => {
-  databaseManager = await CreateDatabaseManager(databaseManagerConfig);
-  ruleRes = {
-    id: '901@1.0.0',
-    cfg: '1.0.0',
-    subRuleRef: '.00',
-    reason: '',
-  };
-});
-
-afterAll(() => {
-  databaseManager.quit();
-});
-
-const determineOutcome = (value: number, ruleConfig: RuleConfig, ruleResult: RuleResult): RuleResult => {
-  if (value != null) {
-    if (ruleConfig.config.bands)
-      for (const band of ruleConfig.config.bands) {
-        if ((!band.lowerLimit || value >= band.lowerLimit) && (!band.upperLimit || value < band.upperLimit)) {
-          ruleResult.subRuleRef = band.subRuleRef;
-          ruleResult.reason = band.reason;
-          break;
-        }
-      }
-  } else throw new Error('Value provided undefined, so cannot determine rule outcome');
-  return ruleResult;
+const ruleRes: RuleResult = {
+  id: '902@1.0.0',
+  cfg: '1.0.0',
+  subRuleRef: '',
+  reason: '',
 };
 
-describe('Transaction amount check', () => {
-  test('Should respond with .x01: Transaction amount exceeds 200', async () => {
-    const req = getMockRequest(250); // Amount is greater than 200
+describe('Rule-902 Tests', () => {
+  let req: RuleRequest;
 
-    const res = await handleTransaction(req, determineOutcome, ruleRes, loggerService, ruleConfig, databaseManager);
-
-    expect(res).toEqual(JSON.parse('{"id":"901@1.0.0", "cfg":"1.0.0","subRuleRef":".x01","reason":"Transaction amount exceeds 200"}'));
+  beforeEach(() => {
+    req = getMockRequest(0); // Her testten önce mock request sıfırlanır
   });
 
-  test('Should proceed with further checks if amount is 200 or less', async () => {
-    const req = getMockRequest(150); // Amount is less than or equal to 200
-    const mockQueryFn = jest.fn();
-    const mockBatchesAllFn = jest.fn().mockResolvedValue([[1]]);
-    databaseManager._pseudonymsDb.query = mockQueryFn.mockResolvedValue({
-      batches: {
-        all: mockBatchesAllFn,
-      },
+  test('Should trigger alert when amount exceeds 100', async () => {
+    req = getMockRequest(150); // Tutar 150 olarak ayarlanır
+    const res = await handleTransaction(req, ruleRes, loggerService, ruleConfig);
+
+    expect(res).toEqual({
+      ...ruleRes,
+      subRuleRef: '.ALT',
+      reason: 'Transaction amount exceeds 100',
     });
+  });
 
-    const res = await handleTransaction(req, determineOutcome, ruleRes, loggerService, ruleConfig, databaseManager);
+  test('Should not trigger alert when amount is 100 or less', async () => {
+    req = getMockRequest(100); // Tutar 100 olarak ayarlanır
+    const res = await handleTransaction(req, ruleRes, loggerService, ruleConfig);
 
-    expect(res).toEqual(
-      JSON.parse('{"id":"901@1.0.0", "cfg":"1.0.0","subRuleRef":".01","reason":"The debtor has performed one transaction to date"}'),
-    );
+    expect(res).toEqual(ruleRes); // Alert tetiklenmez, orijinal sonuç döner
+  });
+
+  test('Should not trigger alert when amount is below 100', async () => {
+    req = getMockRequest(50); // Tutar 50 olarak ayarlanır
+    const res = await handleTransaction(req, ruleRes, loggerService, ruleConfig);
+
+    expect(res).toEqual(ruleRes); // Alert tetiklenmez, orijinal sonuç döner
   });
 });
--- src/rule-756.ts
+++ src/rule-756.ts
@@ -1,107 +1,37 @@
 // SPDX-License-Identifier: Apache-2.0
-
-import { aql, type DatabaseManagerInstance, type LoggerService, type ManagerConfig } from '@frmscoe/frms-coe-lib';
-import { type OutcomeResult, type RuleConfig, type RuleRequest, type RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
-import { unwrap } from '@frmscoe/frms-coe-lib/lib/helpers/unwrap';
+import { type LoggerService } from '@frmscoe/frms-coe-lib';
+import { type RuleConfig, type RuleRequest, type RuleResult } from '@frmscoe/frms-coe-lib/lib/interfaces';
 
 export async function handleTransaction(
   req: RuleRequest,
-  determineOutcome: (value: number, ruleConfig: RuleConfig, ruleResult: RuleResult) => RuleResult,
   ruleRes: RuleResult,
   loggerService: LoggerService,
   ruleConfig: RuleConfig,
-  databaseManager: DatabaseManagerInstance<ManagerConfig>,
 ): Promise<RuleResult> {
-  const context = `Rule-${ruleConfig?.id ? ruleConfig.id : '<unresolved>'} handleTransaction()`;
+  const context = `Rule-${ruleConfig?.id ? ruleConfig.id : '<unresolved>'} handleSimpleAlert()`;
   const msgId = req.transaction.FIToFIPmtSts.GrpHdr.MsgId;
 
-  loggerService.trace('Start - handle transaction', context, msgId);
+  loggerService.trace('Start - handle simple alert', context, msgId);
 
-  // Throw errors early if something we know we need is not provided - Guard Pattern
-  if (!ruleConfig?.config?.bands || !ruleConfig.config.bands.length) {
-    throw new Error('Invalid config provided - bands not provided or empty');
-  }
-  if (!ruleConfig.config.exitConditions) throw new Error('Invalid config provided - exitConditions not provided');
-  if (!ruleConfig.config.parameters) throw new Error('Invalid config provided - parameters not provided');
-  if (!ruleConfig.config.parameters.maxQueryRange) throw new Error('Invalid config provided - maxQueryRange parameter not provided');
-  if (!req.DataCache?.dbtrAcctId) throw new Error('Data Cache does not have required dbtrAcctId');
+  // Step 1: Check if the transaction amount is greater than 100
 
-  // Step 1: Early exit conditions
+  loggerService.trace('Step 1 - Check transaction amount', context, msgId);
 
-  loggerService.trace('Step 1 - Early exit conditions', context, msgId);
+  const transactionAmount = req.transaction.FIToFIPmtSts.TxInfAndSts.ChrgsInf[0].Amt.Amt;
 
-  const UnsuccessfulTransaction = ruleConfig.config.exitConditions.find((b: OutcomeResult) => b.subRuleRef === '.x00');
-
-  if (req.transaction.FIToFIPmtSts.TxInfAndSts.TxSts !== 'ACCC') {
-    if (UnsuccessfulTransaction === undefined) throw new Error('Unsuccessful transaction and no exit condition in config');
+  if (transactionAmount > 100) {
+    loggerService.trace('Transaction amount exceeds threshold, triggering alert', context, msgId);
 
     return {
       ...ruleRes,
-      reason: UnsuccessfulTransaction.reason,
-      subRuleRef: UnsuccessfulTransaction.subRuleRef,
+      subRuleRef: '.ALT', // Indicates that an alert is being generated
+      reason: 'Transaction amount exceeds 100',
     };
   }
 
-  // Step 1.5: Check transaction amount (New Step)
+  // If the transaction amount does not exceed the threshold, return the original result
 
-  loggerService.trace('Step 1.5 - Transaction amount check', context, msgId);
+  loggerService.trace('End - handle simple alert, no alert triggered', context, msgId);
 
-  // Access the amount from the first ChrgsInf object in TxInfAndSts
-  const transactionAmount = req.transaction.FIToFIPmtSts.TxInfAndSts.ChrgsInf[0]?.Amt.Amt;
-  const amountLimit = 200;
-
-  if (transactionAmount > amountLimit) {
-    ruleRes = {
-      ...ruleRes,
-      reason: `Transaction amount exceeds ${amountLimit}`,
-      subRuleRef: '.x01', // Assign an appropriate subRuleRef for the rule-executer
-    };
-    // Instead of returning here, we continue processing
-  }
-
-  // Step 2: Query Setup
-
-  loggerService.trace('Step 2 - Query setup', context, msgId);
-
-  const currentPacs002TimeFrame = req.transaction.FIToFIPmtSts.GrpHdr.CreDtTm;
-  const debtorAccountId = `accounts/${req.DataCache.dbtrAcctId}`;
-  const debtorAccIdAql = aql`${debtorAccountId}`;
-  const maxQueryRange: number = ruleConfig.config.parameters.maxQueryRange as number;
-  const maxQueryRangeAql = aql` AND DATE_TIMESTAMP(${currentPacs002TimeFrame}) - DATE_TIMESTAMP(pacs002.CreDtTm) <= ${maxQueryRange}`;
-
-  const queryString = aql`FOR pacs002 IN transactionRelationship
-    FILTER pacs002._to == ${debtorAccIdAql}
-    AND pacs002.TxTp == 'pacs.002.001.12'
-    ${maxQueryRangeAql}
-    AND pacs002.CreDtTm <= ${currentPacs002TimeFrame}
-    COLLECT WITH COUNT INTO length
-  RETURN length`;
-
-  // Step 3: Query Execution
-
-  loggerService.trace('Step 3 - Query execution', context, msgId);
-
-  const numberOfRecentTransactions = await (await databaseManager._pseudonymsDb.query(queryString)).batches.all();
-
-  // Step 4: Query post-processing
-
-  loggerService.trace('Step 4 - Query post-processing', context, msgId);
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const count = unwrap(numberOfRecentTransactions);
-
-  if (count == null) {
-    // 0 is a legal value
-    throw new Error('Data error: irretrievable transaction history');
-  }
-
-  if (typeof count !== 'number') {
-    throw new Error('Data error: query result type mismatch - expected a number');
-  }
-
-  // Return control to the rule-executer for rule result calculation
-
-  loggerService.trace('End - handle transaction', context, msgId);
-
-  return determineOutcome(count, ruleConfig, ruleRes);
+  return ruleRes;
 }
